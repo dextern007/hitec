845baa05248b42ac22f938b34f1044fcc59e28c1
diwaharSmart <smartdiwahar@outlook.com>
2022-07-13 17:40:58 +0530

    update

---
+++ odoo/addons/legion_enterprise_theme/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Profesional Theme',
+    'version': '14.0.1.0.1',
+    'summary': 'Profesional Theme',
+    'author': 'AaitPro',
+    'license': 'AGPL-3',
+    'maintainer': 'AaitPro',
+    'company': 'AaitPro',
+    'website': 'https://aaitpro.com',
+    'depends': [
+        'web'
+    ],
+    'category':'Branding',
+    'description': """
+           AaitPro Theme
+    """,
+   'data': [
+
+    'views/webclient_template_extend.xml',
+
+    ],
+    'price':0,
+    'currency':'USD',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': []
+}
